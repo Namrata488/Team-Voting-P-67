536199169e00b2277bf08064441622fa79752d3b
Namrata488 <namrata3622@gmail.com>
2022-10-08 21:14:49 +0530

    Added firebase config and made changes in team Info

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+    apiKey: "AIzaSyArAgYVjEP4jTk-sATS6j7YVykgo5CZVpE",
+    authDomain: "proj67-967fe.firebaseapp.com",
+    projectId: "proj67-967fe",
+    storageBucket: "proj67-967fe.appspot.com",
+    messagingSenderId: "27254228238",
+    appId: "1:27254228238:web:25e16747ee5c518063835f",
+    measurementId: "G-FKL25G04VZ"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
